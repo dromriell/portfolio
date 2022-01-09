82e0a76f07fe5df265e03b3d0bb79fd54b1f91d7
dromriell <devonromriell@gmail.com>
2022-01-08 23:15:51 -0500

    - Remove redundant or unused methods - Create custom event - Add comments

--- src/scrollManager/ScrollManager.js
+++ src/scrollManager/ScrollManager.js
@@ -1,25 +1,40 @@
-import ProjectScreen from "./ProjectScreen";
 import ScrollSection from "./ScrollSection";
 
+/**
+ * Creates and manages full page, vertical and/or horizontal scrolling.
+ *
+ * @param {number} scrollDelay The delay before allowing another scroll execution. Default is 700ms.
+ * @param {string} scrollClassY The CSS class for each vertical scroll section.
+ * @param {string} scrollClassX The CSS class for each horizontal scroll section.
+ */
 export default class ScrollManager {
-  constructor(projectsNode, projectData, scrollDelay = 700) {
+  constructor(
+    scrollDelay = 700,
+    scrollClassY = "scrollSectionY",
+    scrollClassX = "scrollSectionX"
+  ) {
     this.scrollDelay = scrollDelay;
+    this.scrollClassY = scrollClassY;
+    this.scrollClassX = scrollClassX;
+
     this.isScrolling = false;
+    this.isScrollLocked = false;
     this.isXScrollScreenFocused = false;
     this.currentScreenIndex = 0;
     this.maxScreenIndex = 0;
+
     this.scrollOrderArray = [];
-    this.worksElementArray = [];
     this.xScrollElArray = [];
 
-    this.projectsNode = projectsNode;
-    this.projectData = projectData;
-    this.projectElementArray = document.createDocumentFragment();
+    this.scrollEvent = new CustomEvent("scrollEvent", {
+      detail: {
+        index: this.currentScreenIndex,
+        isXScroll: this.isXScrollScreenFocused,
+      },
+    });
 
     this.onScroll = () => {};
-
-    this.renderProjectData();
-    this.setVerticalScrollOrder();
+    this.setScrollOrder();
 
     // Handlers
     this.handlers = {
@@ -51,32 +66,15 @@ export default class ScrollManager {
     window.addEventListener("keydown", this.handlers.scrollKey, false);
   }
 
-  renderProjectData() {
-    /**
-     * Renders list of project data into a nodeList of individual project screens which is then
-     * inserted into the worksScreen list div node. Auto formats based on the data received.
-     */
-    for (const [index, project] of this.projectData.entries()) {
-      const projectElement = new ProjectScreen(index, project);
-
-      this.worksElementArray.push(`#project-${index}`);
-      this.projectElementArray.appendChild(projectElement.nodes.article);
-      this.projectsNode.appendChild(this.projectElementArray);
-    }
-  }
-
-  setVerticalScrollOrder() {
-    /**
-     * Sets the vertical scroll order by selecting all elements with the
-     * scrollSectionY class. The length of the new array determines the
-     * limit of vertical scrolling. Also adds any section with a
-     * scrollSectionX class into the state xScrollElArray for horizontal
-     * scroll handling.  Immediately calls setHorizontalScroll before exiting.
-     */
-    const scrollSections = document.querySelectorAll(".scrollSectionY");
+  /**
+   * Creates a ScrollSection instance for each element containing the scrollClassY and determines the scroll direction based on
+   * if scrollClassX is present.
+   */
+  setScrollOrder() {
+    const scrollSections = document.querySelectorAll(`.${this.scrollClassY}`);
 
     for (const [index, section] of scrollSections.entries()) {
-      const isXScroll = section.classList.contains("scrollSectionX");
+      const isXScroll = section.classList.contains(this.scrollClassX);
       const scrollSection = new ScrollSection(index, section, isXScroll);
 
       if (isXScroll) {
@@ -88,24 +86,26 @@ export default class ScrollManager {
     this.maxScreenIndex = this.scrollOrderArray.length - 1;
   }
 
-  setNextIndex(delta) {
-    /**
-     * Handles setting the next screen index based on delta from scroll event.
-     * Assumes next index will be within range.
-     */
+  /**
+   * Sets the next screen index based on delta from a given scroll event.
+   * Assumes next index will be within range. Only used to track vertical
+   * scroll index and is only called within handleVerticalScroll.
+   * @param {number} scrollDelta The directional value of a user scroll input.
+   * @returns {number} Returns the index for the next section to scroll to.
+   */
+  setNextIndex(scrollDelta) {
     const prevIndex = this.currentScreenIndex;
-    const scrollDirection = delta < 0 ? -1 : 1;
+    const scrollDirection = scrollDelta < 0 ? -1 : 1;
     const nextIndex = prevIndex + scrollDirection;
     this.currentScreenIndex = nextIndex;
     return nextIndex;
   }
 
+  /**
+   * Handles either the call to scroll horizontally or vertically.
+   * @param {number} scrollDelta The directional value of a user scroll input.
+   */
   executeScroll(scrollDelta) {
-    /**
-     * Passes scrollDelta to either handleHorizontalScroll or handleVerticalScroll
-     * based on if isXScrollScreenFocused. This then updates the header bar display
-     * before calling the scroll input timeout.
-     */
     this.isScrolling = true;
 
     if (this.isXScrollScreenFocused) {
@@ -113,16 +113,18 @@ export default class ScrollManager {
     } else {
       this.handleVerticalScroll(scrollDelta);
     }
+    window.dispatchEvent(this.scrollEvent);
+
     this.onScroll();
     this.handleScrollTimeout();
   }
 
+  /**
+   * Handles the page scroll to the main site sections. Exits the function if
+   * the next index will be out of the screen index range.
+   * @param {number} scrollDelta The directional value of a user scroll input.
+   */
   handleVerticalScroll(scrollDelta) {
-    /**
-     * Handles the page scroll to the main site sections. Exits the function if
-     * the next index will be out of the screen index range.
-     */
-
     if (
       (this.currentScreenIndex === 0 && scrollDelta < 0) ||
       (this.currentScreenIndex === this.maxScreenIndex && scrollDelta > 0)
@@ -131,22 +133,21 @@ export default class ScrollManager {
       return;
     }
     const nextIndex = this.setNextIndex(scrollDelta);
-    const nextScrollPosition =
-      this.scrollOrderArray[nextIndex].element.getBoundingClientRect().y;
+    const nextSection = this.scrollOrderArray[nextIndex];
+    const nextScrollPosition = nextSection.element.getBoundingClientRect().y;
+    this.isXScrollScreenFocused = nextSection.isXScroll;
     window.scrollBy({
       top: nextScrollPosition,
       behavior: "smooth",
     });
-    // menu.setFocusedButton(nextIndex);
   }
 
+  /**
+   * Handles scrolling along x-axis on relevant sections. Calls handleVerticalScroll if
+   * the next index is outside of horizontal scroll range.
+   * @param {number} scrollDelta The directional value of a user scroll input.
+   */
   handleHorizontalScroll(scrollDelta) {
-    /**
-     * Handles vertical scrolling for the works screen. Exits early if the
-     * next index is out of works index bounds and calls handleVerticalScroll
-     * to complete the appropriate scroll away from the works screen. Creates
-     * a psuedo scroll by updating classes and using CSS transitions and style.
-     */
     const currentSection = this.xScrollElArray.find(
       (element) => element.index === this.currentScreenIndex
     );
@@ -163,20 +164,17 @@ export default class ScrollManager {
     currentSection.clearXScrollOrderClasses();
     currentSection.setXScrollOrderClasses(nextIndex);
     currentSection.currentIndex = nextIndex;
-
-    // if (infoMenu.state.isOpen) {
-    //   // Close the info menu to allow text updates for next description
-    //   infoMenu.toggleMenu();
-    // }
   }
 
+  /**
+   * Handles scrolls directly to a given vertical section using its index.
+   * Only used with vertical scroll sections.
+   * @param {number} index The index of a vertical section to scroll to.
+   */
   handleDirectScroll(index) {
-    /**
-     * Handles scrolls directly to a given index. Used with the menu to jump direct
-     * to a section.
-     */
     this.isScrolling = true;
     this.currentScreenIndex = index;
+    const targetSection = this.scrollOrderArray[index];
     const nextScrollPosition =
       this.scrollOrderArray[index].element.getBoundingClientRect().y;
     window.scrollBy({
@@ -188,48 +186,38 @@ export default class ScrollManager {
     this.handleScrollTimeout();
   }
 
+  /**
+   * Resets the x scroll sections to either first index or max index depending on
+   * the given index param. Used for resetting order when jumping straight to a
+   * section, i.e. menu navigation.
+   * @param {number} index The index of the next vertical section to scroll to.
+   */
   setXScrollSectionsIndexOrder(index) {
-    /**
-     * Resets the x scroll sections to either first index or max index depending on
-     * the given index param. Used for resetting order when jumping straight to a
-     * section, i.e. menu navigation.
-     */
     this.xScrollElArray.map((element) => {
-      const { children, maxIndex, currentIndex } = element;
       element.clearXScrollOrderClasses();
-      let currentXScrollElement = undefined;
       if (index < element.index) {
-        children[0].classList.add("currentXScroll");
-        children[1].classList.add("nextXScroll");
+        element.setXScrollOrderClasses(0);
         element.currentIndex = 0;
-        currentXScrollElement = children[0];
       } else {
-        children[maxIndex - 1].classList.add("prevXScroll");
-        children[maxIndex].classList.add("currentXScroll");
-        element.currentIndex = maxIndex;
-        currentXScrollElement = children[maxIndex];
-      }
-      const currentXScrollDisplayElements =
-        currentXScrollElement.querySelectorAll(".projectDisplay");
-      for (const displayElement of currentXScrollDisplayElements) {
-        displayElement.classList.add("currentDisplay");
+        element.setXScrollOrderClasses(element.maxIndex);
+        element.currentIndex = element.maxIndex;
       }
     });
   }
 
+  /**
+   * Set the time out between allowed scroll requests.
+   */
   handleScrollTimeout() {
-    /**
-     *
-     */
     const scrollTimeout = setTimeout(() => {
       this.isScrolling = false;
     }, this.scrollDelay);
   }
 
+  /**
+   * Prevents default scrolling from mousewheel and calls executeScroll function.
+   */
   handleWheelEvent(e) {
-    /**
-     * Prevents default scrolling from mousewheel and calls executeScroll function.
-     */
     e.stopPropagation();
     e.preventDefault();
     // if (this.isScrolling || this.menu.state.isOpen) {
@@ -241,12 +229,12 @@ export default class ScrollManager {
     }
   }
 
+  /**
+   * Prevent touch scrolling and trigger executeScroll if touch delta is greater than 20.
+   * This allows for touches on buttons and prevents unintended scrolling.
+   */
   handleTouchEvent(e) {
-    /**
-     * Prevent touch scrolling and trigger executeScroll if touch delta is greater than 20.
-     * This allows for touches on buttons and prevents unintended scrolling.
-     */
-    if (this.state.isOpen || this.isScrolling) {
+    if (this.isScrollLocked || this.isScrolling) {
       return;
     } else if (e.type === "touchstart") {
       this.touchStart = e.changedTouches[0].screenY;
@@ -266,13 +254,13 @@ export default class ScrollManager {
     e.stopImmediatePropagation();
   }
 
+  /**
+   * Prevents default scrolling from scroll keys and calls executeScroll function.
+   * Key codes are as follows:
+   * spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36,
+   * left: 37, up: 38, right: 39, down: 40,
+   */
   handleScrollKeyEvent(e) {
-    /**
-     * Prevents default scrolling from scroll keys and calls executeScroll function.
-     * Key codes are as follows:
-     * spacebar: 32, pageup: 33, pagedown: 34, end: 35, home: 36,
-     * left: 37, up: 38, right: 39, down: 40,
-     */
     if (this.isScrolling) {
       e.stopPropagation();
       e.preventDefault();
