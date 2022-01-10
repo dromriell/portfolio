6a4989052c3fa98433dd01fe10318c2540e93e79
dromriell <devonromriell@gmail.com>
2022-01-09 23:12:21 -0500

    - Change test data to reflect individual sections

--- src/testData.js
+++ src/testData.js
@@ -4,37 +4,19 @@
 export const worksData = [
   // TEST DATA
   {
-    type: "app",
-    name: "ace run",
-    tech: ["react-native, django, drf, postgresql"],
+    name: "About Me",
     description:
       "A complete disc golf tracking experience connected to the PDGA API. Search for and add discs to your bag and track your shot history across thousands of courses.",
-    privacy: "private",
-    img: "static/Screenshot_1632242642.png",
+    img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
   },
   {
-    type: "combo",
-    name: "ts dash",
-    tech: ["react, react-native, django, drf, postgresql"],
+    name: "Projects",
     description:
       "An unofficial app/site to help Tile Shop employees track daily work tasks and design appointment.",
-    privacy: "private",
-    comboTypes: [
-      {
-        type: "site",
-        img: "https://cdn.pixabay.com/photo/2017/03/10/10/07/bathroom-2132342_960_720.jpg",
-      },
-      {
-        type: "app",
-        img: "https://cdn.pixabay.com/photo/2017/06/22/16/56/fractal-2431532_960_720.jpg",
-      },
-    ],
-    img: null,
+    img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
   },
   {
-    type: "site",
-    name: "test site",
-    tech: ["react, django-rest-framework, SQLite"],
+    name: "Tile Design",
     description:
       "A test website for making sure that the layouts here are flexible enough and display correctly. Needs to be replaced with an actual project when one is available.",
     img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
