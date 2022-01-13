0ac3be48004b498d214b2e01caddb57cff0cea66
dromriell <devonromriell@gmail.com>
2022-01-12 22:32:54 -0500

    - Change img src data - Change About Me to About

--- src/testData.js
+++ src/testData.js
@@ -4,21 +4,21 @@
 export const worksData = [
   // TEST DATA
   {
-    name: "About Me",
+    name: "About",
     description:
       "A complete disc golf tracking experience connected to the PDGA API. Search for and add discs to your bag and track your shot history across thousands of courses.",
-    img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
+    img: "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   },
   {
-    name: "Projects",
+    name: "Develope",
     description:
       "An unofficial app/site to help Tile Shop employees track daily work tasks and design appointment.",
-    img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
+    img: "https://images.pexels.com/photos/438382/pexels-photo-438382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   },
   {
-    name: "Tile Design",
+    name: "& Design",
     description:
       "A test website for making sure that the layouts here are flexible enough and display correctly. Needs to be replaced with an actual project when one is available.",
-    img: "https://cdn.pixabay.com/photo/2015/10/29/14/38/web-1012467_960_720.jpg",
+    img: "https://images.pexels.com/photos/8357333/pexels-photo-8357333.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
   },
 ];
