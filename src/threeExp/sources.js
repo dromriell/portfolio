0daa83db95654c93342da26a09730cbd096e7143
dromriell <devonromriell@gmail.com>
2022-01-18 14:30:45 -0500

    - Add parent property for event handling - Add bathroom scene sources

--- src/threeExp/sources.js
+++ src/threeExp/sources.js
@@ -1,21 +1,48 @@
 import tilesBakedTextureURL from "../../static/textures/tilesBaked.jpg";
 import floorBakedTextureURL from "../../static/textures/floorBaked.jpg";
 import tilesModelURL from "../../static/models/stackTiles.glb?url";
+import bathroomBakedTextureURL from "../../static/textures/bathroomBaked.jpg";
+import bathroomParticleTextureURL from "../../static/textures/12.png";
+import bathroomModelURL from "../../static/models/bathroom.glb?url";
 
-export default [
+export const tileSources = [
   {
     name: "tilesBakedTexture",
     type: "texture",
     path: tilesBakedTextureURL,
+    parent: "tile",
   },
   {
     name: "floorBakedTexture",
     type: "texture",
     path: floorBakedTextureURL,
+    parent: "tile",
   },
   {
     name: "tilesModel",
     type: "gltfModel",
     path: tilesModelURL,
+    parent: "tile",
+  },
+];
+
+export const bathroomSources = [
+  {
+    name: "bakedTexture",
+    type: "texture",
+    path: bathroomBakedTextureURL,
+    parent: "bathroom",
+  },
+  {
+    name: "particleTexture",
+    type: "texture",
+    path: bathroomParticleTextureURL,
+    parent: "bathroom",
+  },
+  {
+    name: "model",
+    type: "gltfModel",
+    path: bathroomModelURL,
+    parent: "bathroom",
   },
 ];
