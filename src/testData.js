e3d96d9c24b04bde457f326c060e0bc3ba625def
dromriell <devonromriell@gmail.com>
2022-01-21 23:05:10 -0500

    - Change image sources

--- src/testData.js
+++ src/testData.js
@@ -1,3 +1,5 @@
+import aboutPicURL from "../static/aboutPic.png";
+
 /**
  * Test Data
  */
@@ -7,13 +9,13 @@ export const worksData = [
     name: "About",
     description:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
-    img: "https://images.pexels.com/photos/167699/pexels-photo-167699.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    img: aboutPicURL,
   },
   {
     name: "Develope",
     description:
       "An unofficial app/site to help Tile Shop employees track daily work tasks and design appointment.",
-    img: "https://images.pexels.com/photos/438382/pexels-photo-438382.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    img: "https://images.pexels.com/photos/4884118/pexels-photo-4884118.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     apps: [
       {
         name: "Test App",
@@ -57,6 +59,6 @@ export const worksData = [
     name: "& Design",
     description:
       "A test website for making sure that the layouts here are flexible enough and display correctly. Needs to be replaced with an actual project when one is available.",
-    img: "https://images.pexels.com/photos/8357333/pexels-photo-8357333.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    img: undefined,
   },
 ];
