ad891344c5fb4dfcce31877d8630b301c2a094d4
dromriell <devonromriell@gmail.com>
2022-01-23 23:39:09 -0500

    - Change aboutPic source - Change develope background source

--- src/testData.js
+++ src/testData.js
@@ -1,4 +1,4 @@
-import aboutPicURL from "../static/aboutPic.png";
+import aboutPicURL from "../static/aboutPic2.png";
 
 /**
  * Test Data
@@ -15,7 +15,7 @@ export const worksData = [
     name: "Develope",
     description:
       "An unofficial app/site to help Tile Shop employees track daily work tasks and design appointment.",
-    img: "https://images.pexels.com/photos/4884118/pexels-photo-4884118.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    img: "https://images.pexels.com/photos/1759240/pexels-photo-1759240.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
     apps: [
       {
         name: "Test App",
