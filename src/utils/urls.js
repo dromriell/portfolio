b7594b0ff287a024f0e20692a8e7dec6f8dc0497
dromriell <devonromriell@gmail.com>
2022-01-26 21:44:31 -0500

    - Change base url to local network IP

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,8 @@
 import aboutPicURL from "../../static/aboutPic3.png";
 
 export const apiUrls = {
-  screens: "http://127.0.0.1:8000/api/v1/screens/list/",
-  noteCreate: "http://127.0.0.1:8000/api/v1/notes/create",
+  screens: "http://192.168.4.39:8000/api/v1/screens/list/",
+  noteCreate: "http://192.168.4.39:8000/api/v1/notes/create",
 };
 
 export const staticUrls = {
