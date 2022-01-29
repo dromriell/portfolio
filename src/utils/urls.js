654ebde6bf68f8e4817cf2ac9a3cf57e5738fc95
dromriell <devonromriell@gmail.com>
2022-01-28 23:09:05 -0500

    - Add vhsOverlay gif url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,5 +1,6 @@
 import aboutPicURL from "../../static/aboutPic6.png";
 import developePicURL from "../../static/lightbulb.png";
+import vhsURL from "../../static/vhs.gif";
 
 export const apiUrls = {
   screens: "https://dromriell-api.herokuapp.com/api/v1/screens/list/",
@@ -9,4 +10,5 @@ export const apiUrls = {
 export const staticUrls = {
   aboutPic: aboutPicURL,
   developePic: developePicURL,
+  vhsOverlay: vhsURL,
 };
