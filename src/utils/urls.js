c3c57994055ddb517583cbac79353454653d6997
dromriell <devonromriell@gmail.com>
2022-01-28 22:50:53 -0500

    - Add developePic static url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,5 @@
-import aboutPicURL from "../../static/aboutPic3.png";
+import aboutPicURL from "../../static/aboutPic6.png";
+import developePicURL from "../../static/lightbulb.png";
 
 export const apiUrls = {
   screens: "https://dromriell-api.herokuapp.com/api/v1/screens/list/",
@@ -7,4 +8,5 @@ export const apiUrls = {
 
 export const staticUrls = {
   aboutPic: aboutPicURL,
+  developePic: developePicURL,
 };
