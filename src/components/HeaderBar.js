0ada55b3d1ab607913aa533b930dd8fd9fd749fa
dromriell <devonromriell@gmail.com>
2022-06-14 11:01:12 -0400

    - Fix issue with mobile menu toggle click/touch - Add touchstart event

--- src/components/HeaderBar.js
+++ src/components/HeaderBar.js
@@ -51,9 +51,14 @@ export default class HeaderBar {
         <rect y="60" width="100" height="20"></rect>
       </svg>`;
 
-    this.mobileMenuToggle.addEventListener("click", () => {
-      this.toggleMobileMenu();
+    ["touchstart", "click"].forEach((event) => {
+      this.mobileMenuToggle.addEventListener(
+        event,
+        () => this.toggleMobileMenu(),
+        false
+      );
     });
+
     this.menuOverlay.classList.add("menuOverlay");
     this.vhsOverlay.style.background = `url(${staticUrls.vhsOverlay})`;
     this.vhsOverlay.style.backgroundSize = "cover";
