458e9a375380220f8971c70a9f879a8d9114a373
dromriell <devonromriell@gmail.com>
2022-06-17 23:09:16 -0400

    - Add background filter to create more foreground contrast

--- src/scrollManager/ParallaxItem.js
+++ src/scrollManager/ParallaxItem.js
@@ -1,13 +1,20 @@
 export default class ParallaxItem {
   constructor(data, index) {
-    this.imgSrc = data.img_source;
+    this.imgSrc = data.bg_img_source;
     this.backgroundColor = data.background_color;
     this.element = document.createElement("div");
     this.img = document.createElement("img");
+    this.isAppScreen = data.is_app_screen;
 
     this.img.src = this.imgSrc;
     if (this.backgroundColor) {
       this.element.style.background = this.backgroundColor;
+    }
+
+    if (this.isAppScreen) {
+      const backgroundFilterNode = document.createElement("div");
+      backgroundFilterNode.classList.add("backgroundFilter");
+      this.element.appendChild(backgroundFilterNode);
     }
 
     this.element.classList.add("parralaxItem");
