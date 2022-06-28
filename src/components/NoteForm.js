ee7a6b3ae01214e8f4bc61f485c3e0882b070790
dromriell <devonromriell@gmail.com>
2022-06-28 16:55:21 -0400

    - Add additional form validation checks

--- src/components/NoteForm.js
+++ src/components/NoteForm.js
@@ -6,6 +6,9 @@ export default class NoteForm {
     this.inputs = this.form.querySelectorAll("input, textarea");
     this.button = this.form.querySelector("button");
     this.isSubmitable = true;
+    this.isFormValid = false;
+    this.postLimit = 1;
+    this.postCount = 0;
 
     this.name = "";
     this.email = "";
@@ -13,13 +16,23 @@ export default class NoteForm {
 
     this.form.addEventListener("submit", async (e) => {
       e.preventDefault();
-      if (!this.isSubmitable) {
-        return;
-      }
       const submittedFormData = new FormData(e.target);
       this.name = submittedFormData.get("name");
       this.email = submittedFormData.get("email");
       this.note = submittedFormData.get("note");
+
+      this.validateForm();
+
+      if (
+        !this.isSubmitable ||
+        !this.isFormValid ||
+        this.postCount >= this.postLimit
+      ) {
+        this.name = "";
+        this.email = "";
+        this.note = "";
+        return;
+      }
 
       try {
         const noteResponse = await postNote(this.name, this.email, this.note);
@@ -33,7 +46,25 @@ export default class NoteForm {
     });
   }
 
+  validateForm() {
+    let isFormValid = true;
+    console.log(this.name.length);
+    if (this.name.length < 1 || this.name.length > 75) {
+      isFormValid = false;
+    }
+    if (this.email.length < 1 || this.email.length > 75) {
+      isFormValid = false;
+    }
+    if (this.note.length < 1 || this.note.length > 350) {
+      isFormValid = false;
+    }
+    this.isFormValid = isFormValid;
+    return isFormValid;
+  }
+
   disableForm() {
+    document.querySelector(".formButtonGroup button").disabled = true;
+    this.postCount = this.postCount + 1;
     this.isSubmitable = false;
     this.inputs.forEach((input) => (input.readOnly = true));
     this.button.disabled = true;
